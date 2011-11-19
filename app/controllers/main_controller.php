713c268e00b821efdbacc15861f4e874735ca670
Andrew Vigotsky <avigotsky@gmail.com>
2011-11-19 12:06:54 -0500

    Cleaned up a little...

--- app/controllers/main_controller.php
+++ app/controllers/main_controller.php
@@ -3,28 +3,7 @@ class MainController {
   var $default = "home";
   
   function home() {
-    global $midpts;
-    $midpts = Property::getPubMidPts();  
-  }
-  
-  function contact() {
-    global $message;
-    global $passed;
-    $passed = false;
-    $to = "blink182av@gmail.com";
-    if(!empty($_POST['contact']['name'])) {
-      if(!empty($_POST['contact']['email'])) {
-        if(!empty($_POST['contact']['subject'])) {
-          if(!empty($_POST['contact']['message'])) {
-            if(mail($to, $_POST['contact']['subject'], $_POST['contact']['message'], 'From: ' . $_POST['contact']['name'] . ' <' . $_POST['contact']['email'] . '>')) {
-              $message = "Thank you for your email!  You can respect a response within the next 48 hours.<br><br>Regards,<br>The Pistilli Staff";
-              $passed = true;
-              echo "worked";
-            } 
-          }
-        }
-      }
-    }
+    
   }
   
   function getDefault() {
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,10 +5,10 @@ define("DB_TO_LOAD", "main");
 require_once(DOC_ROOT . "vendor/plugins/sfYaml/sfYamlParser.php");
 require_once(DOC_ROOT . "vendor/plugins/ActiveRecord/ActiveRecord.php");
 require_once(DOC_ROOT . "vendor/plugins/recaptchalib.php");
-require_once(DOC_ROOT . "vendor/plugins/wkhtmltopdf/wkhtmltopdf.php");
+#require_once(DOC_ROOT . "vendor/plugins/wkhtmltopdf/wkhtmltopdf.php");
 #require_once(DOC_ROOT . "vendor/plugins/html2pdf/html2pdf.class.php");
 require_once(DOC_ROOT . "vendor/plugins/swift-mailer/lib/swift_required.php");
-require_once(DOC_ROOT . "vendor/plugins/phmagick/phmagick.php");
+#require_once(DOC_ROOT . "vendor/plugins/phmagick/phmagick.php");
 define("PUBLIC_KEY", "6Lepf8YSAAAAAPz7h7TtyfNplS3mhEfkIi7Hw2pA");
 define("PRIVATE_KEY", "6Lepf8YSAAAAAAGOQqb-bv89C4Qt_fOloZt7T8tc");
 
